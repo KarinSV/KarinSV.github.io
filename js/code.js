404119c7877b976e6781c02862c8145a78a8a81d
KarinSV <85022398+KarinSV@users.noreply.github.com>
2022-03-29 21:07:16 +0200

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,2 +1,1 @@
-window.alert("Hi! I'm Karina. This is my project.");
-window.alert("Welcome to WEBBUTVECKLING 1 Uppdrag 5!");
+window.alert("Hi! I'm Karina. This is my project for course WEBBUTVECKLING 1.");
